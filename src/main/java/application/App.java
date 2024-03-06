394907983090188aa9792ce41afe2a0a3905e0ca
abdelaziz-amil <abdelaziz.amil@etudiant.univ-brest.fr>
2024-03-06 17:58:57 +0100

    add : pour voir ce qu'il se passe

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -2,12 +2,25 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class App {
+	private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+		Environment env = context.getEnvironment();
+
+		LOGGER.info("Datasource URL: " + env.getProperty("spring.datasource.url"));
+		LOGGER.info("Datasource Username: " + env.getProperty("spring.datasource.username"));
+		LOGGER.info("Datasource Password: " + env.getProperty("spring.datasource.password"));
 	}
 
 }
